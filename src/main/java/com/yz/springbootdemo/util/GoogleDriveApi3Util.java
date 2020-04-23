c23c9da5d58273f4f752f684d8cfdfa1a12faf41
yanzhu <>
2020-04-23 11:31:30 +0800

    删除不用代码

--- src/main/java/com/yz/springbootdemo/util/GoogleDriveApi3Util.java
+++ src/main/java/com/yz/springbootdemo/util/GoogleDriveApi3Util.java
@@ -32,8 +32,20 @@ public class GoogleDriveApi3Util {
      * If modifying these scopes, delete your previously saved tokens/ folder.
      * 【注意】：改变后要删除 /tokens/StoredCredential
      */
-    private static final List<String> SCOPES = Collections.singletonList(DriveScopes.DRIVE_FILE);
+    private static final List<String> SCOPES = Collections.singletonList(DriveScopes.DRIVE);
     private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
+
+    // 构建driver
+    private Drive service;
+    {
+        try {
+            // Build a new authorized API client service.
+            final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+            service = new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT)).setApplicationName(APPLICATION_NAME).build();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     /**
      * Creates an authorized Credential object.
@@ -50,21 +62,17 @@ public class GoogleDriveApi3Util {
         GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
 
         // Build flow and trigger user authorization request.
-        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
                 .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
-                .setAccessType("offline")
-                .build();
+                .setAccessType("offline").build();
 
-        // 方法1
+        // 方法1：需要输入校验码
 //        String url = flow.newAuthorizationUrl().setRedirectUri(REDIRECT_URI).build();
 //        System.out.println("Please open the following URL in your browser then type the authorization code:");
 //        System.out.println("  " + url);
 //        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 //        String code = br.readLine();
-//        GoogleTokenResponse response = flow
-//                .newTokenRequest(code)
-//                .setRedirectUri(REDIRECT_URI).execute();
+//        GoogleTokenResponse response = flow.newTokenRequest(code).setRedirectUri(REDIRECT_URI).execute();
 //        return flow.createAndStoreCredential(response, null);
 
         // 方法2
@@ -72,60 +80,13 @@ public class GoogleDriveApi3Util {
         return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
     }
 
-    public static Drive getDriveService() {
-        Drive service = null;
-        try {
-            // 构建driver
-            final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-            service = new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
-                    .setApplicationName(APPLICATION_NAME)
-                    .build();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return service;
-    }
-
-    public static void covert2Html() {
-        try {
-            Drive service = getDriveService();
-            // 上传文件
-            File fileMetadata = new File();
-            fileMetadata.setName("My Report");
-            //fileMetadata.setMimeType("application/vnd.google-apps*");
-            fileMetadata.setMimeType("application/html");
-            // 指定文件
-            String name = String.format("/Users/yanzhu/Downloads/其他/%s", "江苏省江都中学2019-2020学年下学期高一数学周练试卷2020.3.22（无答案）.docx");
-            java.io.File filePath = new java.io.File(name);
-            FileContent mediaContent = new FileContent("application/html", filePath);
-            File file = service.files()
-                    .create(fileMetadata, mediaContent)
-                    .setFields("id")
-                    .execute();
-            System.out.println("File ID: " + file.getId());
-            // 下载文件
-            OutputStream outputStream = new ByteArrayOutputStream();
-//            service.files().export(file.getId(), "application/html").executeMediaAndDownloadTo(outputStream);
-
-            //OutputStream outputStream = new FileOutputStream("/Users/yanzhu/Downloads/t.html");// 指定目录
-            service.files().get(file.getId()).executeMediaAndDownloadTo(outputStream);// get方式
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void getFiles() throws Exception {
-        // Build a new authorized API client service.
-        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-        Drive service = new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
-                .setApplicationName(APPLICATION_NAME)
-                .build();
-
+    /**
+     * 查询文件
+     * @throws Exception
+     */
+    public void getFiles() throws Exception {
         // Print the names and IDs for up to 10 files.
-        FileList result = service.files().list()
-                .setPageSize(10)
-                .setFields("nextPageToken, files(id, name)")
-                .execute();
+        FileList result = service.files().list().setPageSize(10).setFields("nextPageToken, files(id, name)").execute();
         List<File> files = result.getFiles();
         if (files == null || files.isEmpty()) {
             System.out.println("No files found.");
@@ -137,8 +98,37 @@ public class GoogleDriveApi3Util {
         }
     }
 
+    /**
+     * 上传并下载文件：实现文件类型转换
+     */
+    public void covert2Html() {
+        try {
+            // 上传文件
+            File fileMetadata = new File();
+            fileMetadata.setName("My Report");
+            // csv fileMetadata.setMimeType("application/vnd.google-apps.spreadsheet");
+            fileMetadata.setMimeType("application/vnd.google-apps.document");
+            // fileMetadata.setMimeType("application/HTML");
+            // 指定文件
+            String name = String.format("/Users/yanzhu/Downloads/其他/%s", "江苏省江都中学2019-2020学年下学期高一数学周练试卷2020.3.22（无答案）.docx");
+            java.io.File filePath = new java.io.File(name);
+            // csv FileContent mediaContent = new FileContent("text/csv", filePath);
+            FileContent mediaContent = new FileContent("application/vnd.openxmlformats-officedocument.wordprocessingml.document", filePath);
+            File file = service.files().create(fileMetadata, mediaContent).setFields("id").execute();
+            System.out.println("File ID: " + file.getId());
+            // 下载文件
+            // OutputStream outputStream = new ByteArrayOutputStream();
+            OutputStream outputStream = new FileOutputStream("/Users/yanzhu/Downloads/t.html");// 备用方法：指定目录
+            service.files().export(file.getId(), "text/html").executeMediaAndDownloadTo(outputStream);
+            // service.files().get(file.getId()).executeMediaAndDownloadTo(outputStream);// // 备用方法：get方式
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String... args) throws Exception {
-        covert2Html();
-        //getFiles();
+        GoogleDriveApi3Util util = new GoogleDriveApi3Util();
+        util.covert2Html();
+        //util.getFiles();
     }
 }
